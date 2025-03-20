2440b5c0ade1b842bceefff8d58403a47e52d6bd
Samuel Hilbert <samuelthilbert@gmail.com>
2025-03-20 18:45:55 -0400

    Most of wireframe complete
    Created the home page, and login/logout rudementary system with cookies. Will need to implement using the server at some point, but this should be good for now.

---
+++ Site/main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# main.py
+# Author: Samuel Hilbert
+#-----------------------------------------------------------------------
+
+import sys
+import flask
+
+#-----------------------------------------------------------------------
+
+app = flask.Flask(__name__, template_folder='.')
+
+@app.route('/', methods=['GET'])
+def home():
+    global startUp
+    if startUp:
+        print("Start Up happened")
+        startUp = False
+        rendered = flask.render_template('index.html', log=False)
+        response = flask.make_response(rendered)
+        response.set_cookie('log', "False")
+        return response
+    print('home was run')
+    logged_in = str_to_bool(flask.request.cookies.get('log', None))
+    return flask.render_template('index.html', log=logged_in)
+
+@app.route('/login', methods=['GET'])
+def login():
+    rendered =  flask.render_template('login.html', log = True)
+    response = flask.make_response(rendered)
+    response.set_cookie('log', 'True')
+    return response
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    redirect = flask.redirect('/')
+    response = flask.make_response(redirect)
+    response.set_cookie('log', 'False')
+    return response
+
+def str_to_bool(string):
+    if string == "True":
+        return True
+    return False
+
+def main():
+    try:
+        global startUp
+        startUp = True
+        app.run(host='0.0.0.0', port = 8000, debug=True)
+    except Exception as ex:
+        print(ex, file=sys.stderr)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
