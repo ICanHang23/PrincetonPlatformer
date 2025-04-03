634804db3555d43344fdcd7b8e31106a2357599e
Owen Yu <owenyu107@gmail.com>
2025-04-03 18:42:18 -0400

    try

--- Assets/Scripts/UploadScore.cs
+++ Assets/Scripts/UploadScore.cs
@@ -5,22 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class UploadScore : MonoBehaviour
 {
-    public static UploadScore Instance{
-        get;
-        set;
-    }
+    public static UploadScore Instance { get; private set; }
     private static string url = "http://localhost:5000/receivescore";
 
     [SerializeField] GameData data;
 
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;  // Set the singleton instance
+        }
+        else
+        {
+            Destroy(gameObject);  // Ensure only one instance exists
+        }
+    }
+
     public static void SendScore(int deaths, float time)
     {
+        if (Instance == null)
+        {
+            Debug.LogError("UploadScore instance is null. Make sure the UploadScore script is attached to an active GameObject.");
+            return;
+        }
+
         string level = SceneManager.GetActiveScene().name;
         Instance.StartCoroutine(UploadScoreCoroutine(level, deaths, time));
     }
 
-    private static IEnumerator UploadScoreCoroutine(string level, int deaths, float time){
-
+    private static IEnumerator UploadScoreCoroutine(string level, int deaths, float time)
+    {
         ScoreData scoreData = new ScoreData
         {
             level = level,
@@ -28,17 +43,16 @@ public class UploadScore : MonoBehaviour
             time = time
         };
 
-        //Converting to JSON
+        // Converting to JSON
         string jsonData = JsonUtility.ToJson(scoreData);
 
-        //SEnding over
+        // Sending over
         using (UnityWebRequest www = new UnityWebRequest(url, "POST"))
         {
             www.SetRequestHeader("Content-Type", "application/json");
             byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(jsonData);
             www.uploadHandler = new UploadHandlerRaw(jsonToSend);
             www.downloadHandler = new DownloadHandlerBuffer();
-            
 
             // Wait for the request to complete
             yield return www.SendWebRequest();
