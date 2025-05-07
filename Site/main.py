128fa874dbdc9900e2ea5701f4671d32fe6fd7e9
Samuel Hilbert <samuelthilbert@gmail.com>
2025-05-07 15:57:24 -0400

    Improved footer and fixed css on the about page

--- Site/main.py
+++ Site/main.py
@@ -68,13 +68,15 @@ def login():
 @app.route('/game', methods=['GET'])
 def game():
     logged_in = flask.session.get('logged_in', False) 
-    rendered = flask.render_template('game.html', log=logged_in)
+    username = flask.session.get('username', None)
+    rendered = flask.render_template('game.html', log=logged_in,
+                                    username = username)
     response = flask.make_response(rendered)
     response.headers['Content-Encoding'] = 'brotli'
     utils.set_last_page('/game')
     return response
 
-# I'm pretty sure post is right here to receive data?
+# Grab score from the game and send it to the DB
 @app.route('/receivescore', methods = ['POST'])
 def receivescore():
     data = flask.request.get_json()
@@ -106,9 +108,6 @@ def receivescore():
 
 @app.route('/logout', methods=['GET'])
 def logout():
-    #Using session here allows us to clear all data
-    #This means that we don't have a hard logout yet.
-    #A user logged in will always be until logged out manually.
     flask.session.clear()
     return flask.redirect('/')
 
@@ -146,6 +145,7 @@ def leaderboard():
     logged_in = flask.session.get('logged_in', False) 
     lvl = flask.request.args.get('lvl', None)
     pg = int(flask.request.args.get('pg', 1))
+    username =  flask.session.get('username', None)
 
     if lvl == None:
         return flask.redirect('/')
@@ -159,7 +159,7 @@ def leaderboard():
     utils.set_last_page('/leaderboard?lvl=%s&pg=%s' % (lvl, pg))
     return flask.render_template('leaderboard.html', table = table_info,
                                 lvl = lvl, pg = pg, limit = limit, log=logged_in,
-                                netid_index = 0, runid_index = 3)
+                                netid_index = 0, runid_index = 3, username = username)
 
 @app.route('/about', methods=['GET'])
 def about_page():
@@ -173,6 +173,7 @@ def about_page():
 def times(user):
     logged_in = flask.session.get('logged_in', False)
     pg = int(flask.request.args.get('pg', 1))
+    username =  flask.session.get('username', None)
 
     table_info = query_times(user)
 
@@ -183,8 +184,8 @@ def times(user):
 
     ref = utils.get_last_page()
     return flask.render_template('leaderboard.html', table = table_info,
-                                username = user, pg = pg, limit = limit,
-                                log=logged_in, ref = ref,
+                                user = user, pg = pg, limit = limit,
+                                log=logged_in, ref = ref, username = username,
                                 netid_index = 5, runid_index = 0)
 
 # gets called upon clicking the "watch" button in the leaderboard
